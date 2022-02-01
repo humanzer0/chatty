041af55ead9e871ae0ccae60ecd8ade035a0fca1
Andrey Burmagin <a.burmagin@smartshell.gg>
2022-02-01 18:41:44 +0300

    Move core::Peer definitions into source file

--- core/include/chatty/core/peer.h
+++ core/include/chatty/core/peer.h
@@ -9,53 +9,10 @@ class Peer {
 public:
     Peer(boost::asio::io_context& io_context,
          const boost::asio::ip::address& chat_room,
-         const std::string& nickname)
-        : socket_(io_context)
-        , room_endpoint_(chat_room, chatty_port_)
-        , nickname_(nickname)
-    {
-        socket_.open(room_endpoint_.protocol());
-        socket_.set_option(boost::asio::ip::udp::socket::reuse_address(true));
-        socket_.bind(room_endpoint_);
+         const std::string& nickname);
 
-        socket_.set_option(boost::asio::ip::multicast::join_group(chat_room));
-
-        auto welcome_message = std::string(nickname_ + " connected to the chat");
-
-        socket_.async_send_to(boost::asio::buffer(welcome_message), room_endpoint_,
-                              [this](const boost::system::error_code& error_code, std::size_t bytes_sent){
-            if (!error_code){
-                std::cout << "Entered chat room successfully" << std::endl;
-            }
-        });
-    }
-
-    void do_receive(){
-        socket_.async_receive_from(boost::asio::buffer(receiving_buffer_), remote_endpoint_,
-                              [this](const boost::system::error_code& error_code, std::size_t bytes_received){
-            if (!error_code.failed() && bytes_received > 0){
-                auto received_message_string = std::string(receiving_buffer_.begin(), receiving_buffer_.begin() + bytes_received);
-                if (received_message_string.find(nickname_) != 0){
-                    std::cout.write(receiving_buffer_.data(), bytes_received);
-                    std::cout << "" << std::endl;
-                }
-                do_receive();
-            }
-        });
-    }
-
-    void do_send(){
-        std::string nickname = nickname_;
-        std::string message;
-        std::getline(std::cin, message);
-        std::string buffer = nickname.append(": " + message);
-        socket_.async_send_to(boost::asio::buffer(buffer, maximum_message_size_), room_endpoint_,
-                           [this, message](const boost::system::error_code& /*error_code*/, std::size_t bytes_sent){
-            std::cout << "You: " << message << std::endl;
-            std::cout << "";
-            do_send();
-        });
-    }
+    void do_receive();
+    void do_send();
 
 private:
     constexpr static std::uint16_t chatty_port_ = 8000U;
---
+++ core/src/peer.cpp
@@ -0,0 +1,55 @@
+#include "chatty/core/peer.h"
+
+namespace core {
+
+Peer::Peer(boost::asio::io_context& io_context,
+           const boost::asio::ip::address& chat_room,
+           const std::string& nickname)
+    : socket_(io_context)
+    , room_endpoint_(chat_room, chatty_port_)
+    , nickname_(nickname)
+{
+    socket_.open(room_endpoint_.protocol());
+    socket_.set_option(boost::asio::ip::udp::socket::reuse_address(true));
+    socket_.bind(room_endpoint_);
+
+    socket_.set_option(boost::asio::ip::multicast::join_group(chat_room));
+
+    auto welcome_message = std::string(nickname_ + " connected to the chat");
+
+    socket_.async_send_to(boost::asio::buffer(welcome_message), room_endpoint_,
+                          [this](const boost::system::error_code& error_code, std::size_t bytes_sent){
+        if (!error_code){
+            std::cout << "Entered chat room successfully" << std::endl;
+        }
+    });
+}
+
+void Peer::do_receive(){
+    socket_.async_receive_from(boost::asio::buffer(receiving_buffer_), remote_endpoint_,
+                          [this](const boost::system::error_code& error_code, std::size_t bytes_received){
+        if (!error_code.failed() && bytes_received > 0){
+            auto received_message_string = std::string(receiving_buffer_.begin(), receiving_buffer_.begin() + bytes_received);
+            if (received_message_string.find(nickname_) != 0){
+                std::cout.write(receiving_buffer_.data(), bytes_received);
+                std::cout << "" << std::endl;
+            }
+            do_receive();
+        }
+    });
+}
+
+void Peer::do_send(){
+    std::string nickname = nickname_;
+    std::string message;
+    std::getline(std::cin, message);
+    std::string buffer = nickname.append(": " + message);
+    socket_.async_send_to(boost::asio::buffer(buffer, maximum_message_size_), room_endpoint_,
+                       [this, message](const boost::system::error_code& /*error_code*/, std::size_t bytes_sent){
+        std::cout << "You: " << message << std::endl;
+        std::cout << "";
+        do_send();
+    });
+}
+
+} // namespace core
